3fb22f85611bba0b9b95211afdf126a5eb82b7a7
johackson <johackson@johacksons-air.lan>
2024-09-20 10:05:37 -0500

    Displace static

--- podrateSite/settings.py
+++ podrateSite/settings.py
@@ -54,6 +54,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+DISABLE_COLLECTSTATIC = 1
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
