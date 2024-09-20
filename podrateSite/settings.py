36b674a4ab6a45b39b68aa8b05793438e4bb772d
johackson <johackson@johacksons-air.lan>
2024-09-20 09:52:24 -0500

    Update env vars

--- podrateSite/settings.py
+++ podrateSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 from botocore.exceptions import ClientError
 
 import boto3
@@ -23,32 +24,36 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 
-def get_secret(secret_name):
-    region_name = "us-east-2"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    return get_secret_value_response['SecretString']
+# def get_secret(secret_name):
+#     region_name = "us-east-2"
+#
+#     # Create a Secrets Manager client
+#     session = boto3.session.Session()
+#     client = session.client(
+#         service_name='secretsmanager',
+#         region_name=region_name
+#     )
+#
+#     try:
+#         get_secret_value_response = client.get_secret_value(
+#             SecretId=secret_name
+#         )
+#     except ClientError as e:
+#         # For a list of exceptions thrown, see
+#         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+#         raise e
+#
+#     return get_secret_value_response['SecretString']
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret("django_podrate_secret_key")
-AWS_ACCESS_KEY_ID = get_secret("aws_access_key_id")
-AWS_SECRET_ACCESS_KEY = get_secret("aws_secret_access_key")
+# SECRET_KEY = get_secret("django_podrate_secret_key")
+# AWS_ACCESS_KEY_ID = get_secret("aws_access_key_id")
+# AWS_SECRET_ACCESS_KEY = get_secret("aws_secret_access_key")
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
